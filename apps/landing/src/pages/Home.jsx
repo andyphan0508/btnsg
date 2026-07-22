906f374d111c807560716d83457e11eb16fad2ab
Andy Phan <anduy0508@gmail.com>
2026-07-23 00:05:47 +0700

    remove temp boarding

--- apps/landing/src/pages/Home.jsx
+++ apps/landing/src/pages/Home.jsx
@@ -1,11 +1,11 @@
-import Hero from '../components/Hero.jsx'
-import Intro from '../components/Intro.jsx'
-import Slider from '../components/Slider.jsx'
-import Board from '../components/Board.jsx'
-import ThemeYear from '../components/ThemeYear.jsx'
-import Schedule from '../components/Schedule.jsx'
-import Ministries from '../components/Ministries.jsx'
-import Contact from '../components/Contact.jsx'
+import Hero from "../components/Hero.jsx";
+import Intro from "../components/Intro.jsx";
+import Slider from "../components/Slider.jsx";
+import Board from "../components/Board.jsx";
+import ThemeYear from "../components/ThemeYear.jsx";
+import Schedule from "../components/Schedule.jsx";
+import Ministries from "../components/Ministries.jsx";
+import Contact from "../components/Contact.jsx";
 
 export default function Home() {
   return (
@@ -14,12 +14,12 @@ export default function Home() {
       <main className="wrap">
         <Intro />
         <Slider />
-        <Board />
+        {/* <Board /> */}
         <ThemeYear />
         <Schedule />
         <Ministries />
         <Contact />
       </main>
     </>
-  )
+  );
 }
